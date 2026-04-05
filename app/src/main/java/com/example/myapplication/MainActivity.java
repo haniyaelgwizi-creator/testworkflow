f525246a7500f4d08912696accfc81f66d9e0d76
HANIYA <haniyaelgwizi@gmail.com>
2026-04-06 00:11:33 +0100

    Initial Android project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHallo;
+    private EditText etName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Liaison XML ↔ Java
+        tvHallo = findViewById(R.id.tvHALLO);
+        etName = findViewById(R.id.eTName);
+    }
+
+    // Méthode appelée quand on clique sur le bouton
+    public void onClick(View view) {
+        if (etName.getText().length() == 0) {
+            tvHallo.setText("Salut");
+        } else {
+            String str = etName.getText().toString();
+            String strr = "HALLO " + str;
+            tvHallo.setText(strr);
+        }
+    }
+}
